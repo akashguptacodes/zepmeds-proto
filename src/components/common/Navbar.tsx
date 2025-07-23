5caacf3075607cfe3dd74c8c3ff4117713d12021
Akash Gupta <akashgupta7484@gmail.com>
2025-07-23 16:26:32 +0530

    zapmeds

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
               <Zap className="h-6 w-6" />
             </div>
             <div>
-              <h1 className="text-2xl font-black bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-display">ZepMeds</h1>
+              <h1 className="text-2xl font-black bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent font-display">ZapMeds</h1>
               <p className="text-xs text-gray-500 font-medium font-mono">15-min delivery</p>
             </div>
           </Link>
